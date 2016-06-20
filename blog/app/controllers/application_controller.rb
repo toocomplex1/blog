8bd09a2c083321b4f3c25bd98eba9a0cac27c9d6
akash mishra <akashmishra30493@gmail.com>
2016-06-20 12:44:54 +0530

    added refactoring

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+	
+  http_basic_authenticate_with name: "akash", password: "secret", except: [:index, :show]
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   
 
   resources :articles do
-  resources :comments
+    resources :comments
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
